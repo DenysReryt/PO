83157fc2b8bcd20d82e723108a98c25fa09f585d
Denys <denysreryt28@gmail.com>
2023-02-03 23:17:43 +0200

    Only Back-End

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+from main import unique_letter
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {'status': 'Working'}
+
+
+@app.post('/home/')
+async def result(text: str):
+    res = unique_letter(text)
+    if res is not None:
+        return unique_letter(text)
+    else:
+        raise HTTPException(status_code=404, detail='No unique letter was found')
+
+
+if __name__ == '__main__':
+    uvicorn.run('app:app', host='localhost', reload=True, port=8000)

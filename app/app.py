b8d74c2a1716fe380dd2e8f4c89464710bf9f007
Denys <denysreryt28@gmail.com>
2023-02-06 21:55:08 +0200

    Add httpredirect

--- app/app.py
+++ app/app.py
@@ -4,17 +4,19 @@ from fastapi.responses import HTMLResponse
 import uvicorn
 from app.main import unique_letter
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
 
-origins = ['http://localhost:8000',
-           'http://0.0.0.0:8000']
+app.add_middleware(HTTPSRedirectMiddleware)
+# origins = ['http://localhost:8000',
+#            'http://0.0.0.0:8000']
 
-app.add_middleware(CORSMiddleware,
-                   allow_origins=origins,
-                   allow_credentials=True,
-                   allow_methods=['*'],
-                   allow_headers=['*'], )
+# app.add_middleware(CORSMiddleware,
+#                    allow_origins=origins,
+#                    allow_credentials=True,
+#                    allow_methods=['*'],
+#                    allow_headers=['*'], )
 
 templates = Jinja2Templates(directory='templates/')
 
